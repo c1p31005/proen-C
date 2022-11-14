9504fbac1149195ed8564bb4a8f4a72c642343d9
Emori Yuto <91306113+20020702@users.noreply.github.com>
2022-11-14 22:11:29 +0900

    Add files via upload

---
+++ sheet9-pro.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="Shift-JIS">
+<title>学校プロフィール</title>
+<link rel="stylesheet" style type="text/css" href="style.css">
+</head>
+<body>
+<h1>学校プロフィール</h1>
+<div class="box-title">
+  <h1>トップページ</h1>
+</div>
+<div class="profile-text">
+  <input type="profile" name="profile" placeholder="ユーザ情報"></br>
+</div>
+<div class="back-btm">
+  <button onclick="location.href='sheet6-top.html'">戻る</button>
+</div>
+<?php
+$dsn = "mysql:dbname=proe;host=localhost";
+$my = new PDO($dsn, "proe", "proe");
+$sql = "SELECT * FROM 学校;";
+$st = $my->prepare($sql);
+$st->execute();
+$html = "<table border='1'><tr><th>学校ID</th><th>学校名</th><th>住所</th><th>電話番号</th><th>種目</th><th>パスワード</th><th>緯度</th><th>経度</th></tr>";
+while($row = $st->fetch(PDO::FETCH_ASSOC)){
+$html .= "<tr>";
+foreach($row as $item) $html .= "<td>{$item}</td>";
+$html .= "</tr>";
+}
+$html .= "</table>";
+echo($html);
+?>
+</body>
+</html>
